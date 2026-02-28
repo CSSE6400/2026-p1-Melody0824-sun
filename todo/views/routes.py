b541b87977b53d39fbb24f191d8ae029266ed2af
Mengdie Sun <mengdie.sun@student.uq.edu.au>
2026-02-28 22:39:01 +1000

    Fix todo endpoints

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -2,6 +2,17 @@ from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
+TODO = {
+    "id": 1,
+    "title": "Watch CSSE6400 Lecture",
+    "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+    "completed": True,
+    "deadline_at": "2026-02-27T18:00:00",
+    "created_at": "2026-02-20T14:00:00",
+    "updated_at": "2026-02-20T14:00:00"
+}
+
+
 @api.route('/health')
 def health():
     return jsonify({"status": "ok"})
@@ -9,54 +20,24 @@ def health():
 
 @api.route('/todos', methods=['GET'])
 def get_todos():
-    return jsonify([])
+    return jsonify([TODO])
+
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
-    return jsonify({
-        "id": id,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2026-02-27T18:00:00",
-        "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00"
-    })
+    return jsonify(TODO)
+
 
 @api.route('/todos', methods=['POST'])
 def create_todo():
-    return jsonify({
-        "id": 1,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2026-02-27T18:00:00",
-        "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00"
-    }), 201
+    return jsonify(TODO), 201
 
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
-    return jsonify({
-        "id": id,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2026-02-27T18:00:00",
-        "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00"
-    })
+    return jsonify(TODO)
 
 
 @api.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
-    return jsonify({
-        "id": id,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2026-02-27T18:00:00",
-        "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00"
-    })
+    return jsonify(TODO)
